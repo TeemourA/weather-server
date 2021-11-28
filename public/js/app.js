21297ff4dce60330b2c695623c643a478cb1298d
TeemourA <taxprogramming@gmail.com>
2021-11-28 19:21:11 +0300

    add form and info layout for user requests

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,37 @@
-fetch('http://localhost:3000/weather?address=Moscow')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// @ts-nocheck
+const weatherSearchForm = document.querySelector('#weather-search-form');
+const weatherSearchInput = document.querySelector('#weather-search-form input');
+
+const weatherSearchMessage = document.querySelector('#message');
+const weatherSearchResult = document.querySelector('#result');
+
+// weatherSearchMessage.textContent = 'message';
+// weatherSearchResult.textContent = 'result';
+
+weatherSearchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  weatherSearchMessage.textContent = 'Loading...';
+  weatherSearchResult.textContent = '';
+
+  fetch(`http://localhost:3000/weather?address=${weatherSearchInput.value}`)
+    .then((res) => res.json())
+    .then(
+      ({ feelslike, place_name, temperature, weather_descriptions, error }) => {
+        if (error) {
+          weatherSearchMessage.textContent = error;
+        } else {
+          weatherSearchMessage.textContent = 'Loaded successfully';
+          console.log({
+            feelslike,
+            place_name,
+            temperature,
+            weather_descriptions,
+            error,
+          });
+          weatherSearchMessage.textContent = place_name;
+          weatherSearchResult.textContent = `Weather conditions: ${weather_descriptions[0]}\nTemperature: ${temperature}C\nFeels like: ${feelslike}C`;
+        }
+      }
+    );
+});
